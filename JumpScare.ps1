4cf65f113bcf79bd41a7b6a702b0d29292e0e6ca
Alek <86006452+AlekCG@users.noreply.github.com>
2023-04-08 21:10:44 -0300

    Update JumpScare.ps1

--- JumpScare.ps1
+++ JumpScare.ps1
@@ -31,7 +31,7 @@
 
 # Download Image; replace link to $image to add your own image
 
-$image =  "https://github.com/AlekCG/Jump-Scare/blob/main/jumpscare.png"
+$image =  "https://github.com/AlekCG/Jump-Scare/blob/main/jumpscare.png?raw=true"
 
 $i = -join($image,"?dl=1")
 iwr $i -O $env:TMP\i.png
